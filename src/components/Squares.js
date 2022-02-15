a358438475c455113b3987850c996a2497eac31f
enhopkin@gmail.com <enhopkin@gmail.com>
2022-02-14 18:13:46 -0700

    by some luck and a help video...

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -24,7 +24,7 @@ export default function Squares() {
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square. On page load there's no active square,
   // so the value of 'activeSquare' should be null.
-const [square, setSquare] = useState(true);
+const [squares, setSquares] = useState(listOfSquareIds);
 const [activeSquare, setActiveSquare] = useState (null)
 
   const getClassName = id => {
@@ -32,7 +32,7 @@ const [activeSquare, setActiveSquare] = useState (null)
     // It should return a string containing the class name of 'active', if the id passed
     // as the argument matches the active square in state, empty string otherwise.
     // Right-click and "inspect element" on the square to see its effect.
-    return ''
+    return id === activeSquare ? 'active' : ''
   };
 
   const markActive = id => {
@@ -40,6 +40,12 @@ const [activeSquare, setActiveSquare] = useState (null)
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
+    // if( id=== activeSquare) {
+    //   setActiveSquare(null)
+    // }else {
+    //   setActiveSquare(id);
+    // }
+    id === activeSquare ? setActiveSquare(null): setActiveSquare(id)
   };
 
   return (
@@ -50,7 +56,7 @@ const [activeSquare, setActiveSquare] = useState (null)
           // Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           // We might say: "it works, though!" But if the list of squares is not state,
           // we could never add squares, change squares or remove squares in the future. Fix!
-          listOfSquareIds.map(id =>
+          squares.map(id =>
             <div
               id={id}
               key={id}
